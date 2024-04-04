e386d9b1af20a94f052a6a283c40d4f554444b07
Szymon Adamiak <adamiak.szymon@gmail.com>
2024-04-04 11:33:20 +0200

    temp solution

--- src/app/components/MainHeader.js
+++ src/app/components/MainHeader.js
@@ -7,7 +7,9 @@ export default async function MainHeader({ blok, text, className = null }) {
       {...storyblokEditable(blok)}
     >
       <div className={`inline-block heading-underline mb-3 ${className || ""}`}>
-        <h1 className="text-black text-h1 font-bold mt-2 pr-4 relative z-[1]">{blok.title}</h1>
+        <h1 className="text-black text-h1 font-bold mt-2 pr-4 relative z-[1]">
+          {blok?.title || text}
+        </h1>
       </div>
     </div>
   );
