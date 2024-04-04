9d857b67de6b1fb0d77a8a9255291ff2ce44345f
Szymon Adamiak <adamiak.szymon@gmail.com>
2024-04-04 11:26:50 +0200

    Update page

--- src/app/components/MainHeader.js
+++ src/app/components/MainHeader.js
@@ -1,8 +1,13 @@
-export default async function MainHeader({ text, className = null }) {
+import { storyblokEditable } from "@storyblok/react/rsc";
+
+export default async function MainHeader({ blok, text, className = null }) {
   return (
-    <div className="max-w-full m-auto px-full max-xl:px-tablet max-sm:px-mobile">
+    <div
+      className="max-w-full m-auto px-full max-xl:px-tablet max-sm:px-mobile"
+      {...storyblokEditable(blok)}
+    >
       <div className={`inline-block heading-underline mb-3 ${className || ""}`}>
-        <h1 className="text-black text-h1 font-bold mt-2 pr-4 relative z-[1]">{text}</h1>
+        <h1 className="text-black text-h1 font-bold mt-2 pr-4 relative z-[1]">{blok.title}</h1>
       </div>
     </div>
   );
--- src/app/components/Page.js
+++ src/app/components/Page.js
@@ -3,14 +3,11 @@ import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
 import MainHeader from "@/app/components/MainHeader";
 
 const Page = ({ blok }) => (
-  <React.Fragment {...storyblokEditable(blok)}>
+  <div {...storyblokEditable(blok)}>
     {blok.body.map((nestedBlok) => {
-      if (nestedBlok.component === "Page title") {
-        return <MainHeader text={nestedBlok.title} key={nestedBlok._uid} />;
-      }
       return <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />;
     })}
-  </React.Fragment>
+  </div>
 );
 
 export default Page;
