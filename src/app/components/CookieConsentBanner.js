c5a5a3fcdf9081a922f7315f712e1abfef2d5452
Szymon Adamiak <adamiak.szymon@gmail.com>
2024-04-16 15:33:16 +0200

    Newsletter snackbar

--- src/app/components/CookieConsentBanner.js
+++ src/app/components/CookieConsentBanner.js
@@ -30,6 +30,21 @@ const CookieConsentBanner = () => {
 
   return (
     <div className="fixed bottom-6 left-[50%] translate-x-[-50%] w-[90%] max-w-[1170px] bg-white rounded-[5px] shadow-tile-shadow border-1 border-[rgba(231, 231, 231, 1)] p-5 z-50">
+      <div className="cursor-pointer absolute top-4 right-4 bg-white pl-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 14 14"
+          fill="none"
+          onClick={() => setShowBanner(false)}
+        >
+          <path
+            d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z"
+            fill="black"
+          />
+        </svg>
+      </div>
       <h5 className="text-h5 mb-[6px] font-bold">Pliki cookie i prywatność</h5>
       <p>
         Na tej stronie używamy plików cookies i podobnych funkcji do przetwarzania informacji o
@@ -40,13 +55,13 @@ const CookieConsentBanner = () => {
       <div className="flex justify-end gap-6 mt-2">
         <Button
           onClick={handleReject}
-          classes="py-[10px] px-8 bg-brand-darkBlue text-white text-[16px] leading-[16px] min-w-[133px]"
+          classes="py-[10px] px-8 bg-themeableColors-darkBlue text-white text-[16px] leading-[16px] min-w-[133px]"
         >
           <div className="relative top-[1px]">Odrzuć wszystkie</div>
         </Button>
         <Button
           onClick={handleAccept}
-          classes="py-[10px] px-8 bg-brand-darkBlue text-white text-[16px] leading-[12px] min-w-[133px]"
+          classes="py-[10px] px-8 bg-themeableColors-darkBlue text-white text-[16px] leading-[12px] min-w-[133px]"
         >
           <div className="relative top-[1px]">Akceptuj wszystkie</div>
         </Button>
--- src/app/components/Newsletter.js
+++ src/app/components/Newsletter.js
@@ -1,7 +1,5 @@
 "use client";
-import Image from "next/image";
-import Link from "next/link";
-import Image1 from "../../../public/tempImages/image22.png";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 import { getIfGreenUrl } from "@/app/utils";
 import Button from "@/app/components/Button";
@@ -9,6 +7,17 @@ import Button from "@/app/components/Button";
 const Newsletter = () => {
   const pathname = usePathname();
   const isGreenUrl = getIfGreenUrl(pathname);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccess(true);
+    setTimeout(() => {
+      setShowSuccess(false);
+    }, 5000);
+    return () => {
+      clearTimeout();
+    };
+  };
   return (
     <div
       className={`text-themeableColors-darkBlue w-full newsletterBg bg-[url('/newsletter_bg.png')] py-[4.5rem] max-sm:py-10 ${isGreenUrl}`}
@@ -43,7 +52,7 @@ const Newsletter = () => {
               />
             </div>
             <Button
-              type="submit"
+              onClick={handleSubmit}
               classes="py-[14px] px-8 bg-themeableColors-darkBlue text-white text-[21px] leading-[16px] ml-4 min-w-[133px] max-md:ml-0"
             >
               <div className="relative top-[1px]">Dołącz</div>
@@ -59,6 +68,50 @@ const Newsletter = () => {
           </div>
         </form>
       </div>
+      {showSuccess && (
+        <div className="fixed top-32 right-10 max-w-[90%] bg-white rounded-[5px] shadow-tile-shadow border border-brand-darkGreen px-6 py-[16px] z-50">
+          <div className="flex items-center gap-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <g clipPath="url(#clip0_2110_8133)">
+                <circle cx="12" cy="12" r="12" fill="#009E59" />
+                <path
+                  d="M9.59844 14.9246L6.44844 11.7746L5.39844 12.8246L9.59844 17.0246L18.5984 8.02461L17.5484 6.97461L9.59844 14.9246Z"
+                  fill="white"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_2110_8133">
+                  <rect width="24" height="24" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+            <h5 className="text-h5 font-bold text-black leading-[16px] relative top-[1px]">
+              Pomyślnie zapisano do newslettera!
+            </h5>
+            <div className="cursor-pointer flex items-center relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 14 14"
+                fill="none"
+                onClick={() => setShowSuccess(false)}
+              >
+                <path
+                  d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z"
+                  fill="black"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
