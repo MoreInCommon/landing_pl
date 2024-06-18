15561a89b33f5cc47e5fdc8667c76ba458ad5741
Szymon Adamiak <adamiak.szymon@gmail.com>
2024-06-18 16:37:22 +0200

    Better quotes

--- src/app/components/QuoteContainer.js
+++ src/app/components/QuoteContainer.js
@@ -1,98 +1,91 @@
 "use client";
+import { useRef } from "react";
 import { storyblokEditable } from "@storyblok/react";
 import { StoryblokComponent } from "@storyblok/react";
 import Slider from "react-slick";
 
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={`${className} before:hidden`}
-      style={{
-        ...style,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 20,
-        height: 20,
-        bottom: 60,
-        top: "auto",
-        left: 200,
-        zIndex: 1,
-      }}
-      onClick={onClick}
-    >
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g id="Icon">
-          <path
-            id="Vector"
-            d="M11.0575 9.88L12.9375 8L20.9375 16L12.9375 24L11.0575 22.12L17.1642 16L11.0575 9.88Z"
-            fill="#1D2330"
-          />
-        </g>
-      </svg>
-    </div>
-  );
-}
-
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={`${className} before:hidden max-sm:slick-hidden`}
-      style={{
-        ...style,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 20,
-        height: 20,
-        bottom: 60,
-        top: "auto",
-        left: 100,
-        zIndex: 1,
-      }}
-      onClick={onClick}
-    >
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M20.9425 9.88L19.0625 8L11.0625 16L19.0625 24L20.9425 22.12L14.8358 16L20.9425 9.88Z"
-          fill="#1D2330"
-        />
-      </svg>
-    </div>
-  );
-}
 const QuoteContainer = ({ blok }) => {
   const currentItems = blok?.quote || [];
+  let sliderRef = useRef(null);
+  const next = () => {
+    sliderRef.slickNext();
+  };
+  const previous = () => {
+    sliderRef.slickPrev();
+  };
   var settings = {
     infinite: true,
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
     appendDots: (dots) => (
-      <div className="bg-black">
-        <ul> {dots} </ul>
+      <div className="flex align-center gap-1">
+        <div
+          className={`max-sm:slick-hidden`}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 1,
+            cursor: "pointer",
+          }}
+          onClick={previous}
+        >
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M20.9425 9.88L19.0625 8L11.0625 16L19.0625 24L20.9425 22.12L14.8358 16L20.9425 9.88Z"
+              fill="#1D2330"
+            />
+          </svg>
+        </div>
+        <div className="relative top-[3px]">
+          <ul> {dots} </ul>
+        </div>
+        <div
+          className={`max-sm:slick-hidden`}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 1,
+            cursor: "pointer",
+          }}
+          onClick={next}
+        >
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="Icon">
+              <path
+                id="Vector"
+                d="M11.0575 9.88L12.9375 8L20.9375 16L12.9375 24L11.0575 22.12L17.1642 16L11.0575 9.88Z"
+                fill="#1D2330"
+              />
+            </g>
+          </svg>
+        </div>
       </div>
     ),
   };
   return (
     <div {...storyblokEditable(blok)} className="quoteSlick">
-      <Slider {...settings} className="mx-6 max-lg:mx-0">
+      <Slider
+        ref={(slider) => {
+          sliderRef = slider;
+        }}
+        {...settings}
+        className="mx-6 max-lg:mx-0"
+      >
         {currentItems.map((item) => (
           <StoryblokComponent key={item._uid} blok={item} />
         ))}
