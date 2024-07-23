6dbf5186818140c14c7d6fcec133676aa952d612
Szymon Adamiak <adamiak.szymon@gmail.com>
2024-07-23 16:42:31 +0200

    fix image

--- src/app/components/ImageComponent.js
+++ src/app/components/ImageComponent.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function ImageComponent({ src, width, height, alt, title }) {
   return (
-    <div className="flex justify-center mb-10 mt-10 max-xl:px-tablet max-sm:px-mobile">
+    <div className="max-w-standard m-auto w-full flex justify-center mb-10 mt-10 max-xl:px-tablet max-sm:px-mobile">
       <img src={src} width={width} height={height} alt={alt || ""} title={title || ""} />
     </div>
   );
