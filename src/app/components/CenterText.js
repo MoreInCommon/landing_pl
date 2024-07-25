f5511e8b40484bbac06e1888fa1874cf7c9ecde4
Szymon Adamiak <adamiak.szymon@gmail.com>
2024-07-25 09:54:47 +0200

    update margin

--- src/app/components/CenterText.js
+++ src/app/components/CenterText.js
@@ -2,7 +2,7 @@
 
 export default function CenterText({ text }) {
   return (
-    <p className="text-black w-full mb-6 text-bodyRegular max-xl:px-tablet max-sm:px-mobile">
+    <p className="text-black w-full mb-4 text-bodyRegular max-xl:px-tablet max-sm:px-mobile">
       {text}
     </p>
   );
