3d3b80faa3d4e494e4840ce6c3b6e79229aa40cc
Szymon Adamiak <adamiak.szymon@gmail.com>
2024-10-15 10:35:55 +0200

    Update sorting

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -3,7 +3,13 @@ import { StoryblokComponent } from "@storyblok/react";
 
 const Projects = ({ blok }) => {
   const copiedProjects = [...blok.project];
-  const sortedProjects = copiedProjects.reverse();
+  const sortedProjects = copiedProjects.sort((a, b) => {
+    return (
+      (a.created_at ? new Date(a.created_at) : new Date()) -
+      (b.created_at ? new Date(b.created_at) : new Date())
+    );
+  });
+  sortedProjects.reverse();
   return (
     <>
       <div className="flex gap-10 flex-wrap mt-20 items-stretch max-w-full m-auto w-full px-full max-xl:px-tablet max-sm:px-mobile max-lg:justify-center">
