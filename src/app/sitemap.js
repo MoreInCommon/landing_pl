709e3b92e1f2b81fc9668e8e45f07fff35ea39fe
Szymon Adamiak <adamiak.szymon@gmail.com>
2024-12-12 21:04:49 +0100

    Add robots and sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,76 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.moreincommon.pl/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://www.moreincommon.pl/o-nas/zespol",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.moreincommon.pl/o-nas/misja",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.moreincommon.pl/o-nas/statut",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.moreincommon.pl/nasze-projekty",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.moreincommon.pl/w-mediach",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.moreincommon.pl/kontakt/nasze-biuro",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.moreincommon.pl/kontakt/pracuj-z-nami",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.moreincommon.pl/kontakt/dla-mediow",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.moreincommon.pl/siedem-segmentow",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.moreincommon.pl/quiz",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.moreincommon.pl/fokus-na-klimat",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+}
