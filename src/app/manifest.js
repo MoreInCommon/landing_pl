21f80b11056eec3ca4e55ce7e6b609c9d0658af7
Szymon Adamiak <adamiak.szymon@gmail.com>
2024-12-18 10:42:33 +0100

    Add manifest json

---
+++ src/app/manifest.js
@@ -0,0 +1,20 @@
+export default function manifest() {
+  return {
+    name: "More in Common Polska",
+    short_name: "MiC Polska",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#224099",
+    description:
+      "More in Common działa na rzecz wzmocnienia demokracji oraz zmniejszania polaryzacji politycznej i światopoglądowej.",
+    lang: "pl",
+    icons: [
+      {
+        src: "/tempImages/image22.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+  };
+}
