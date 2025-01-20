68a25ff3928ac3b08ce87aef8d8d2c3428664473
Szymon Adamiak <adamiak.szymon@gmail.com>
2025-01-20 09:50:02 +0100

    Indexing improvements

--- src/app/components/MainInfo.js
+++ src/app/components/MainInfo.js
@@ -12,10 +12,10 @@ export default function MainInfo({ blok }) {
         className="px-full max-xl:px-tablet max-sm:px-mobile bg-[#F1F1F3] p-8 justify-center mb-12"
         {...storyblokEditable(blok)}
       >
-        <p className="text-brand-darkBlue text-[36px] leading-[36px] font-bold top-1 relative z-[1] text-center">
+        <h1 className="text-brand-darkBlue text-[36px] leading-[36px] font-bold top-1 relative z-[1] text-center">
           {blok?.text}
           <span className="text-brand-blue">.</span>
-        </p>
+        </h1>
       </div>
       <Button
         onClick={() => router.push(`/${blok.button_url.cached_url}`)}
