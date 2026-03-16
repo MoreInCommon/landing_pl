3a7dfdc7610872c17ec36db94beb0f2ab149a42d
Szymon Adamiak <adamiak.szymon@gmail.com>
2026-03-16 12:02:04 +0100

    add align

--- src/app/components/FlexibleImage.js
+++ src/app/components/FlexibleImage.js
@@ -3,9 +3,14 @@ import Image from "next/image";
 const FlexibleImage = ({ blok }) => {
   const src = blok?.image?.filename || blok?.image || "/batory.png";
   const maxHeight = blok?.max_height ? Number(blok.max_height) : 40;
-  const maxWidth = blok?.max_width ? blok.max_width : 220;
+  const maxWidth = blok?.max_width ? Number(blok.max_width) : 220;
+  const align = (blok?.align || blok?.alignment || "center").toLowerCase();
+
+  const justifyClass =
+    align === "left" ? "justify-start" : align === "right" ? "justify-end" : "justify-center";
+
   return (
-    <div className="flex justify-center">
+    <div className={`flex ${justifyClass} py-4`}>
       <div
         style={{
           width: "100%",
